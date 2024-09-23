9ed9d495b35cd7ade76ade4d39b0945b454c9577
Alessio Guarisco <95379435+Aleee-ggr@users.noreply.github.com>
2024-09-23 20:26:30 +0200

    perf: fileinput is faster than subprocess

--- src/modify.py
+++ src/modify.py
@@ -1,33 +1,13 @@
 import sys
-import subprocess
+import fileinput
 
 
 class Modify:
     def __init__(self, v1, v2):
         self.path = "/sys/class/power_supply/BAT0/"
-        self.modify_charge_thresholds(0, 100)
+        self.modify_charge_thresholds("0", "100")
         self.modify_charge_thresholds(v1, v2)
 
     def modify_charge_thresholds(self, v1, v2):
-        try:
-            subprocess.run(
-                [
-                    "sudo",
-                    "bash",
-                    "-c",
-                    f"echo {v1} > {self.path}charge_start_threshold",
-                ],
-                check=True,
-            )
-            subprocess.run(
-                [
-                    "sudo",
-                    "bash",
-                    "-c",
-                    f"echo {v2} > {self.path}charge_stop_threshold",
-                ],
-                check=True,
-            )
-        except subprocess.CalledProcessError:
-            print("Error while modifying thresholds")
-            sys.exit(1)
+        open(self.path + "charge_start_threshold", "w+").write(v1)
+        open(self.path + "charge_stop_threshold", "w+").write(v2)
